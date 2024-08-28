b87e52559d31e62e71458c8d76c60c948ffa538e
Michel Bartels <login@michelbartels.com>
2024-08-28 21:35:27 +0100

    fix codegen

--- bin/main.ml
+++ bin/main.ml
@@ -9,8 +9,8 @@ let () = Parse.parse test
     |> Blocks_to_untyped_ast.convert
     |> Untyped_ast_to_typed_ast.convert
     |> Typed_ast_to_llvm.convert
-let () = Typed_ast_to_llvm.aot_compile ()
 let () = print_endline @@ Llvm.string_of_llmodule Typed_ast_to_llvm.llmodule
+let () = Typed_ast_to_llvm.aot_compile ()
 (*let program = Front_end.front_end test
 let () = print_endline @@ Typed_ast.show_program program
 let _ = Typed_ast_to_llvm.convert program
