44c8917200e319d963e8040c4d7bd2552481bce4
Michel Bartels <login@michelbartels.com>
2024-09-07 19:56:00 +0100

    update compile script

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 unzip -o $1 -d example_code/
 dune exec scratch_compiler
-gcc out.o -L . -lruntime
+cargo build --release --manifest-path ../scratch-compiler-runtime/Cargo.toml
+cp ../scratch-compiler-runtime/target/release/libruntime.a .
+gcc out.o -L . -lruntime -lgcc_s -lutil -lrt -lpthread -lm -ldl -lc
 ./a.out
